e389171790fbf7082ee2dfeaba4651bb3cbc3cd8
Lynx <kp125808@gmail.com>
2026-02-09 19:51:35 +0530

    Week 2 Day 5 Assignment

--- week-2/day-5(MINI-PROJECT)/resultsDocs/formatJson.py
+++ week-2/day-5(MINI-PROJECT)/resultsDocs/formatJson.py
@@ -4,12 +4,12 @@ import json
 with open("crt.sh.json", "r") as f:
     data = json.load(f)
 
-subdomains = set()  # unique subdomains
+subdomains = set()  
 
 for entry in data:
     names = entry.get("name_value", "").split("\n")
     for name in names:
-        subdomains.add(name.strip())  # remove extra spaces
+        subdomains.add(name.strip())  
 
 # Save cleaned list
 with open("crtsh-dmce-clean.txt", "w") as f:
